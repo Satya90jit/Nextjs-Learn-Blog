e9e7f9020240437dea92aebfb025316365b32739
himansupattanaik <himansupattanaik03@gmail.com>
2023-12-17 20:58:54 +0530

    changes

---
+++ src/components/form/Blogform.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const BlogForm = ({ saveBlog }: any) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [image, setImage] = useState("");
+
+    const hanaleAddPost = () => {
+        const newPost = { title, desc, image }
+        saveBlog(newPost)
+    };
+
+    const handleImageChange = (e: any) => {
+        const file = e.target.files[0];
+
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            setImage(imageUrl);
+        }
+    };
+
+    return (
+        <section>
+            <div className="bg-white text-black rounded-lg p-8 max-w-md w-full shadow-md">
+                <input
+                    type="text"
+                    placeholder="Blog Title"
+                    className="w-full mb-4 p-2 border border-gray-300 rounded"
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    placeholder="Blog Description"
+                    className="w-full mb-4 p-2 border border-gray-300 rounded"
+                    onChange={(e) => setDesc(e.target.value)}
+                />
+                <input
+                    type="file"
+                    accept="image/*"
+                    className="w-full mb-4 p-2 border border-gray-300 rounded"
+                    onChange={handleImageChange}
+                />
+                {image &&
+                    <img src={image} className="h-[8rem] w-[8rem] " />
+                }
+
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={hanaleAddPost}
+                >
+                    Add Blog
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default BlogForm;
--- src/pages/[blog].tsx
+++ src/pages/[blog].tsx
@@ -1,15 +1,50 @@
+import React, { useEffect, useState } from "react";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import BlogForm from "@/components/form/Blogform";
+import BlogCard from "@/components/card/BlogCard";
 
-const BlogDetailsPage = () => {
+const BlogPage = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getFromlocalStorage = () => {
+    const storedData: any = JSON.parse(localStorage.getItem('blogData') || "[]")
+    setPosts(storedData)
+    console.log("==> getlocalstorage calling", storedData)
+  }
+
+  useEffect(() => {
+    getFromlocalStorage()
+  }, []) // parameter ( function , deendency)
+
+
+  // []   ...... empty dependency , run the useeffect only once 
+  // [sate]  ... state dependency , run the useeffect when sate change 
+  // [props] ... ... props dependency , run the useeffect when props change
+
+  const saveBlog = (newPost: any) => {
+    const updatedPosts: any = [...posts, newPost];
+    setPosts(updatedPosts);
+    localStorage.setItem('blogData', JSON.stringify(updatedPosts));
+  };
+
+  console.log("==============>rendering")
+
   return (
     <>
       <Navbar />
-      <section className="flex justify-center h-screen bg-blue-600/10">
-        <div className="text-4xl font-bold">MY BLOG DETAILS</div>
-      </section>
+      <section className="flex flex-col items-center justify-center h-[100%] bg-blue-600/10">
+        <div className="text-4xl font-bold mb-8">MY BLOG DETAILS</div>
+        <BlogForm saveBlog={saveBlog} />
+        <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {posts?.map((post, index) => (
+            <BlogCard key={index} post={post} />
+          ))}
+        </div>
+      </section >
       <Footer />
     </>
   );
 };
-export default BlogDetailsPage;
+
+export default BlogPage;

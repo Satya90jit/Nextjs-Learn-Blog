6ead63cfa9e4e370bc19a7c16899f570cf7e8108
Satya90jit <satyajitsahu2002@gmail.com>
2024-03-17 22:04:27 +0530

    Updated

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,47 +1,48 @@
 import React, { useEffect, useState } from "react";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
-import BlogForm from "@/components/form/Blogform";
 import BlogCard from "@/components/card/BlogCard";
+import BlogForm from "@/components/form/Blogform";
 
 const BlogPage = () => {
   const [posts, setPosts] = useState([]);
 
   const getFromlocalStorage = () => {
-    const storedData: any = JSON.parse(localStorage.getItem('blogData') || "[]")
-    setPosts(storedData)
-    console.log("==> getlocalstorage calling", storedData)
-  }
+    const storedData: any = JSON.parse(
+      localStorage.getItem("blogData") || "[]"
+    );
+    setPosts(storedData);
+    console.log("==> getlocalstorage calling", storedData);
+  };
 
   useEffect(() => {
-    getFromlocalStorage()
-  }, []) // parameter ( function , deendency)
+    getFromlocalStorage();
+  }, []); // parameter ( function , deendency)
 
-
-  // []   ...... empty dependency , run the useeffect only once 
-  // [sate]  ... state dependency , run the useeffect when sate change 
+  // []   ...... empty dependency , run the useeffect only once
+  // [sate]  ... state dependency , run the useeffect when sate change
   // [props] ... ... props dependency , run the useeffect when props change
 
   const saveBlog = (newPost: any) => {
     const updatedPosts: any = [...posts, newPost];
     setPosts(updatedPosts);
-    localStorage.setItem('blogData', JSON.stringify(updatedPosts));
+    localStorage.setItem("blogData", JSON.stringify(updatedPosts));
   };
 
-  console.log("==============>rendering")
+  console.log("==============>rendering");
 
   return (
     <>
       <Navbar />
       <section className="flex flex-col items-center justify-center h-[100%] bg-blue-600/10">
-        <div className="text-4xl font-bold mb-8">MY BLOG DETAILS</div>
+        <h1 className="text-4xl font-bold mb-8">MY BLOG DETAILS</h1>
         <BlogForm saveBlog={saveBlog} />
         <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {posts?.map((post, index) => (
             <BlogCard key={index} post={post} />
           ))}
         </div>
-      </section >
+      </section>
       <Footer />
     </>
   );
